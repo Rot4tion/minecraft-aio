473d548387dc4e1b909688689238f8d598b5ae67
rot4tion <nhonhoem0123@gmail.com>
2024-12-21 10:23:54 +0700

    remove footer

--- src/renderer/src/components/admin-panel/admin-panel-layout.tsx
+++ src/renderer/src/components/admin-panel/admin-panel-layout.tsx
@@ -13,20 +13,20 @@ export default function AdminPanelLayout({ children }: { children: React.ReactNo
       <Sidebar />
       <main
         className={cn(
-          'min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300',
+          'min-h-[calc(100vh)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300',
           !settings.disabled && (!getOpenState() ? 'lg:ml-[90px]' : 'lg:ml-72')
         )}
       >
         {children}
       </main>
-      <footer
+      {/* <footer
         className={cn(
           'transition-[margin-left] ease-in-out duration-300',
           !settings.disabled && (!getOpenState() ? 'lg:ml-[90px]' : 'lg:ml-72')
         )}
       >
         <Footer />
-      </footer>
+      </footer> */}
     </>
   )
 }
